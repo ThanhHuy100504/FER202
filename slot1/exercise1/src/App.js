f77360c9739702a03e25bc2f010cf2da5543e32e
Le Tu Thanh Huy <letuthanhhuy@MacBook-Pro-cua-Huy-Le.local>
2025-05-14 16:53:45 +0700

    update slot1

---
+++ slot1/exercise1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <h1>Xin chào tất cả mọi người</h1>
+      <h2>Hello NestJS</h2>
+    </div>
+  );
+}
+
+export default App;
